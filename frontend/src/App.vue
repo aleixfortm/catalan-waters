a7805600a12ae2a3583547a653c1d80a1d76cd77
aleixfortm <aleix.fort45@gmail.com>
2023-07-22 19:14:32 +0200

    added main section to frontend UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/cat-waters.jpeg">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+      <background-image>
+          <div class="main-container">
+            <div class="pollancre">EVOLUTION OF CATALAN WATER LEVELS</div>
+            <div>
+              <img src="./assets/image.jpg" alt="catwaters-logo" class="main-container__img">
+            </div>
+          </div>
+      </background-image>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BackgroundImage from "./components/BackgroundImage.vue"
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    BackgroundImage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body, html {
+  margin: 0;
+  padding: 0;
+  font-family: 'Source Sans Pro', sans-serif;
 }
 
-img {
-  width: 300px;
+.pollancre {
+    text-transform: uppercase;
+    color: rgba(255,255,255,0.839);
+    letter-spacing: 0.225rem;
+    width: calc(100% + 0.225rem);
+    font-size: 1em;
+    line-height: 1.5;
+    font-weight: 300;
+}
+
+.main-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1080px;
+}
+
+.main-container__img {
+  width: 500px;
   height: auto;
+  border-radius: 15px;
 }
 </style>
---
+++ frontend/src/components/BackgroundImage.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="background-container">
+      <!-- Background Image -->
+      <div class="background-image"></div>
+
+      <!-- Noise Pattern -->
+      <div class="background-noise"></div>
+
+      <div class="content">
+        <slot></slot>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+  .background-container {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+  }
+  
+  .background-image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('../assets/background2.jpg');
+    background-size: cover;
+  }
+  
+  .background-noise {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('data:image/svg+xml;charset=utf8,%3Csvg%20viewBox%3D%220%200%20512%20512%22%20width%3D%22512%22%20height%3D%22512%22%20version%3D%221.1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%20%3Cfilter%20id%3D%22noise%22%3E%20%3CfeTurbulence%20type%3D%22fractalNoise%22%20baseFrequency%3D%220.875%22%20result%3D%22noise%22%20%2F%3E%20%3CfeColorMatrix%20type%3D%22matrix%22%20values%3D%220.0390625%200%200%200%200%200%200.03515625%200%200%200%200%200%200.03515625%200%200%200%200%200%200.56640625%200%22%20%2F%3E%20%3C%2Ffilter%3E%20%3Crect%20filter%3D%22url%28%23noise%29%22%20x%3D%220%22%20y%3D%220%22%20width%3D%22512%22%20height%3D%22512%22%20fill%3D%22transparent%22%20opacity%3D%221%22%20%2F%3E%3C%2Fsvg%3E');
+  }
+  
+  .content {
+    position: absolute;
+    top: 50%; /* Center the content vertically */
+    left: 50%; /* Center the content horizontally */
+    transform: translate(-50%, -50%); /* Adjust for centering */
+    text-align: center; /* Center the text inside the div */
+    color: #ffffff; /* Set the text color */
+
+  }
+  </style>

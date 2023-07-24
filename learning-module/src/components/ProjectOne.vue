c09eec0281b45e89b76d67ad5695095c9611b2c2
paugomezm <pgm5060@gmail.com>
2023-07-24 23:25:30 +0200

    playing around with the task

--- learning-module/src/components/ProjectOne.vue
+++ learning-module/src/components/ProjectOne.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="friend-1">{{friendOne}}</div>
-    <div id="friend-2">{{friendTwo}}</div>
-    <div id="friend-3">{{friendThree}}</div>
+    <div id="friend-1" v-if="visible1">{{friendOne}}</div>
+    <div id="friend-2" v-if="visible2">{{friendTwo}}</div>
+    <div id="friend-3" v-if="visible3">{{friendThree}}</div>
 </template>
 <script>
     export default {
@@ -9,8 +9,27 @@
             return {
                 friendOne: "Aleix",
                 friendTwo: "Guillem",
-                friendThree: "Pau"
+                friendThree: "Pau",
+                visible1: true,
+                visible2: true,
+                visible3: true
             }
+        },
+        methods: {
+            makeItVisible() {
+                var ranNum = Math.random() < 0.5 ? true : false;
+                this.visible1 = ranNum;
+                ranNum = Math.random() < 0.5 ? true : false;
+                this.visible2 = ranNum;
+                ranNum = Math.random() < 0.5 ? true : false;
+                this.visible3 = ranNum;
+            }
+        },
+        mounted() {
+            this.makeItVisible();
+            console.log(this.visible1);
+            console.log(this.visible2);
+            console.log(this.visible3);
         }
     }
 </script>

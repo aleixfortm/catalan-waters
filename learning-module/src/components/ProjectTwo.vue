0ff050a2bb2267357fba7dea571c56bd4de87f42
paugomezm <pgm5060@gmail.com>
2023-07-25 22:30:38 +0200

    project 2 extra task completed

--- learning-module/src/components/ProjectTwo.vue
+++ learning-module/src/components/ProjectTwo.vue
@@ -2,12 +2,17 @@
     <button @click="addNumber" class="add">Add</button>
     <div>{{number}}</div>
     <button @click="removeNumber" class="remove">Remove</button>
+    <br><br>
+
+    <input placeholder="Insert your Name" v-model="username">
+    <div>{{ username }}</div>
 </template>
 <script>
     export default {
         data() {
             return {
-                number: 0
+                number: 0,
+                username: ''
             }
         },
         methods: {
@@ -19,7 +24,13 @@
                 if (this.number > 0) {
                     this.number --;
                 }
+            },
+            printToConsole() {
+                console.log(this.username);
             }
+        },
+        mounted() {
+            this.printToConsole();
         }
     }
 </script>
